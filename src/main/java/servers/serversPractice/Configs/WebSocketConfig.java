e0cbcb2a6168e5c13a7d67c10f30a43694b60ff5
Aleksey <134363714+Milak66@users.noreply.github.com>
2026-09-14 15:19:30 +0200

    FunChat 1.1.2

--- src/main/java/servers/serversPractice/Configs/WebSocketConfig.java
+++ src/main/java/servers/serversPractice/Configs/WebSocketConfig.java
@@ -12,18 +12,13 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-
         registry.enableSimpleBroker("/topic");
-
         registry.setApplicationDestinationPrefixes("/app");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-
         registry.addEndpoint("/ws")
-                .setAllowedOrigins(
-                        "http://localhost:5173"
-                );
+                .setAllowedOriginPatterns("*");
     }
 }
